d2b5f39b1c00b2c51a73809f8cb0c1b80101887d
Norkfor <norkfor@gmail.com>
2021-10-07 10:40:16 +0200

    Add project files.

---
+++ Atvalto/Program.cs
@@ -0,0 +1,61 @@
+﻿//©KozRom
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//©KozRom
+namespace Atvalto
+{
+  class Program
+  {
+    //©KozRom
+    static string hexa = String.Empty;
+    static void Main(string[] args)
+    {
+      bool nemjo = false;
+      do
+      {
+        Console.Write("Kérek egy hexadecimális számot: ");
+        nemjo = false;
+        hexa = Console.ReadLine();
+        hexa = hexa.ToUpper();
+        if (hexa == "")
+        {
+          nemjo = true;
+        }
+        foreach (var v in hexa)
+        {
+          if (v > 'F' || v < 'A' && v > '9' || v < '0')
+          {
+            nemjo = true;
+          }
+        }
+      } while (nemjo);
+      //©KozRom
+      List<double> x = new List<double>();
+      double osszeg = 0;
+      for (int i = 0; i < hexa.Length; i++)
+      {
+        x.Add(Convert.ToChar(hexa.ToString().Substring(i, 1)));
+        if (x[i] >= 65 && x[i] <= 70)
+        {
+          x[i] -= 55;
+        }
+        else if (x[i] >= 48 && x[i] <= 57)
+        {
+          x[i] -= 48;
+        }
+      }
+      x.Reverse();
+      for (int i = 0; i < hexa.Length; i++)
+      {
+        osszeg += x[i] * Math.Pow(16, i);
+      }
+      Console.WriteLine(osszeg);
+      Console.ReadKey();
+      //©KozRom
+    }
+  }
+}
+//©KozRom
---
+++ Kemia/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kemia
+{
+  class Program
+  {
+    static List<Elem> elemek = new List<Elem>();
+    static string vegyjel = String.Empty;
+    static void Main(string[] args)
+    {
+      MasodikFeladat();
+      HarmadikFeladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      Console.ReadKey();
+    }
+
+    private static void OtodikFeladat()
+    {
+      bool nemjo = false;
+      do
+      {
+
+        nemjo = false;
+        Console.Write("5. feladat: Kérek egy vegyjelet: ");
+        vegyjel = Console.ReadLine();
+        vegyjel = vegyjel.ToLower();
+
+
+        if (vegyjel.Length > 2 || vegyjel == "")
+        {
+          nemjo = true;
+        }
+        foreach (var v in vegyjel)
+        {
+          if (v > 'z' || v < 'a')
+          {
+            nemjo = true;
+          }
+        }
+      } while (nemjo);
+    }
+
+    private static void NegyedikFeladat()
+    {
+      int db = 0;
+      foreach (var e in elemek)
+      {
+        if (e.Ev == "Ókor")
+        {
+          db++;
+        }
+      }
+      Console.WriteLine("4. feladat: Felfedezések száma az ókorban: {0}", db);
+    }
+
+    private static void HarmadikFeladat()
+    {
+      Console.WriteLine("3. feladat: Elemek száma: {0}", elemek.Count());
+    }
+
+    private static void MasodikFeladat()
+    {
+      StreamReader be = new StreamReader("felfedezesek.csv");
+      be.ReadLine();
+      while (!be.EndOfStream)
+      {
+        string[] a = be.ReadLine().Split(';');
+        elemek.Add(new Elem(a[0], a[1], a[2], int.Parse(a[3]), a[4]));
+      }
+      be.Close();
+
+      Console.WriteLine("2. feladat: adatok beolvasása");
+    }
+  }
+}
